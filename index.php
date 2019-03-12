c07f45b8d31fca572090f0d5b076382000ea61ae
Mouad ZIANI <mouad.ziani1997@gmail.com>
2019-03-12 23:40:21 +0100

    feat: create file and put content into them

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+	if (isset($_POST['submit']) && !empty($_POST['file-name'])) {
+		$fileName = $_POST['file-name'];
+		$content = $_POST['content'];
+		if(!file_exists($fileName)) {
+			touch($fileName);
+			$message = "The file $fileName updated succesfully";
+		} else {
+			$message = "The file $fileName created succesfully";
+		}
+		$oldContent = file_get_contents($fileName);
+		$oldContent .= $content;
+		file_put_contents($fileName, $oldContent);
+	}
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Make directory</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+	<section class="content" style="margin-top: 100px">
+        <div id="container" class="container">
+            <div class="row">
+            	<div class="col-md-6 offset-sm-3">
+					<?php if (isset($message) && !empty($message)): ?>
+						<div class="alert alert-success"><?= $message ?></div>	
+						<br>
+					<?php endif ?>
+					<form method="post">
+						<div class="form-group">
+							<label for="">File Name</label>
+							<input type="text" name="file-name" class="form-control" placeholder="File name">
+						</div>
+						<div class="form-group">
+							<label for="">Content</label>
+							<textarea name="content" class="form-control" cols="30" rows="10" placeholder="Content"></textarea>
+						</div>
+						<button name="submit" class="btn btn-primary">Create / Update file</button>
+					</form>
+				</div>
+            </div>
+        </div>
+	</section>
+</body>
+</html>
